722ef594a6850b59ccfc78937d14a7ca4755301b
barkerkade <kmbrules33@gmail.com>
2024-01-17 05:27:19 -0700

    Project 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade? ");
+        string valueFromUser = Console.ReadLine();
+
+        int x = int.Parse(valueFromUser);
+        
+        if (x >= 90)
+        {
+            Console.WriteLine("You have an A.");
+        }
+        else if (x >= 80)
+        {
+            Console.WriteLine("You have a B.");
+        }
+        else if (x >= 70)
+        {
+            Console.WriteLine("You have a C.");
+        }
+        else if (x >= 60)
+        {
+            Console.WriteLine("You have a D.");
+        }
+        else if (x < 60)
+        {
+            Console.WriteLine("You have an F.");
+        }
+
+        if (x >= 70)
+        {
+            Console.WriteLine("You have a passing grade.");
+        }
+        else if (x < 70)
+        {
+            Console.WriteLine("You are currently failing the class.");
+        }
+
     }
 }
