1f244c6860cd66b545fe75cf6dd7e1eac17f6c03
barkerkade <kmbrules33@gmail.com>
2024-02-28 09:51:01 -0700

    Working

--- prove/Develop04/Activity.cs
+++ prove/Develop04/Activity.cs
@@ -1,4 +1,21 @@
+
 public class Activity
 {
+    protected string countdown;
+    
+    protected void DisplaySpinner(int duration)
+    {
+        DateTime startTime = DateTime.Now;
+        DateTime futureTime = startTime.AddSeconds(duration);
+        while(DateTime.Now < futureTime)
+        {
+            Console.Write("|");
+            Thread.Sleep(1000);
+            Console.Write("\b \b");
+            Console.Write("/");
+            Console.Write("\b \b");
+            
+        }
 
+    }
 }
--- prove/Develop04/Listing.cs
+++ prove/Develop04/Listing.cs
@@ -1,6 +1,4 @@
-using System.Diagnostics;
-
 class Listing : Activity
 {
-
+    
 }
