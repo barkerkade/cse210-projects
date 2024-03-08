e78178aa096167ae7047a2bf00e618fd232fab56
barkerkade <kmbrules33@gmail.com>
2024-03-07 18:43:16 -0700

    Working

--- prove/Develop04/Activity.cs
+++ prove/Develop04/Activity.cs
@@ -1,12 +1,32 @@
 
+using System.Security.Cryptography.X509Certificates;
+
 public class Activity
 {
     protected string countdown;
+
+    protected int activityDuration;
+
+    protected string description;
+
+    protected string activityTitle;
     
+    public Activity (string description, string activityTitle)
+    {
+       this.description = description; 
+       this.activityTitle = activityTitle;
+    }
+
+    public void DisplayIntro()
+    {
+        Console.WriteLine($"Welcome to the {activityTitle} \n\n {description}");
+    }
     protected void DisplaySpinner(int duration)
     {
         DateTime startTime = DateTime.Now;
         DateTime futureTime = startTime.AddSeconds(duration);
+        
+
         while(DateTime.Now < futureTime)
         {
             Console.WriteLine(" Get ready:");
--- prove/Develop04/Breathing.cs
+++ prove/Develop04/Breathing.cs
@@ -1,4 +1,7 @@
 class Breathing : Activity
 {
-    
+    public Breathing() : base("This activity is for controlled breathing", "Breathing Activity")
+    {
+        
+    }
 }
